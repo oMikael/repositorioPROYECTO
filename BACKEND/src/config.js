07e3fae6829bc4c33532617b0537c0a047361e5c
oMikael <30442513+oMikael@users.noreply.github.com>
2019-05-11 21:42:47 -0500

    10

--- BACKEND/src/config.js
+++ BACKEND/src/config.js
@@ -1,3 +1,4 @@
-module.exports.connectionURL = 'mongodb+srv://mongodb+srv://admin1:1234@cluster0-skgbz.mongodb.net/test?retryWrites=true'
+module.exports.connectionURL = 'mongodb+srv://admin1:1234@cluster0-skgbz.mongodb.net/test?retryWrites=true'
+								
 
 module.exports.secret = 'suSuperSecret'
--- BACKEND/src/db/mongoose.js
+++ BACKEND/src/db/mongoose.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 if(process.env.NODE_ENV === 'production'){
 	var connectionURL = process.env.connectionURL
 } else {
-	var connectionURL = 'mongodb+srv://admin1:1234@cluster0-skgbz.mongodb.net/test?retryWrites=true'
+	const secret = require('../config.js')
+	var connectionURL = secret.connectionURL
+	//mongodb+srv://admin1:1234@cluster0-skgbz.mongodb.net/test?retryWrites=true
 	//mongodb+srv://admin:taquitos@cluster0-xg9bk.mongodb.net/clase?retryWrites=true
 }
 
